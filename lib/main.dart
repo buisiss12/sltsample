13217d2b6e995eecffc371b1c1378919e8753319
nyny <buisiss12@gmail.com>
2024-01-04 19:46:23 +0900

    firebaseデフォルトの構築完了flutter run問題なし

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:sltsampleapp/firebase_options.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
