6223b59608cdf75e0477c922392ef93d1a9f5e44
nyny <buisiss12@gmail.com>
2024-01-06 00:42:18 +0900

    ログイン画面⇨会員登録画面遷移実装

--- lib/resistration_page.dart
+++ lib/resistration_page.dart
@@ -1,32 +1,31 @@
 // ignore_for_file: avoid_print
 
-import 'resistration_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+class RegistrationPage extends StatefulWidget {
+  const RegistrationPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<RegistrationPage> createState() => _RegistrationPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _RegistrationPageState extends State<RegistrationPage> {
   final _phoneNumber = TextEditingController();
   final _passWord = TextEditingController();
-  bool _hidePassword = false;
-  bool _isLoginButton = false; // ボタンの有効状態を追跡
+  bool _hidePassword = true;
+  bool _isResistrationButton = false;
 
   @override
   void initState() {
     super.initState();
-    _phoneNumber.addListener(_loginButtonState);
-    _passWord.addListener(_loginButtonState);
+    _phoneNumber.addListener(_resistrationButtonState);
+    _passWord.addListener(_resistrationButtonState);
   }
 
-  void _loginButtonState() {
+  void _resistrationButtonState() {
     setState(() {
-      _isLoginButton =
+      _isResistrationButton =
           _phoneNumber.text.isNotEmpty && _passWord.text.isNotEmpty;
     });
   }
@@ -38,7 +37,7 @@ class _LoginPageState extends State<LoginPage> {
     super.dispose();
   }
 
-  void _login() {
+  void _register() {
     print('電話番号: ${_phoneNumber.text}, パスワード: ${_passWord.text}');
   }
 
@@ -49,27 +48,20 @@ class _LoginPageState extends State<LoginPage> {
         FocusScope.of(context).unfocus(); // キーボードを隠す
       },
       child: Scaffold(
+        appBar: AppBar(
+          title: const Text('新規会員登録'),
+        ),
         body: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Flexible(
-                child: Image.asset('assets/images/1080x384solotte.png',
-                    fit: BoxFit.cover),
-              ),
-              const Text('ログイン',
-                  style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold)),
-              const SizedBox(height: 16),
-              const Text('電話番号 または 会員ID',
-                  style: TextStyle(fontWeight: FontWeight.bold)),
+              const Text('電話番号', style: TextStyle(fontWeight: FontWeight.bold)),
               TextField(
                 controller: _phoneNumber,
-                decoration: InputDecoration(
-                  hintText: '電話番号または会員IDを入力(ハイフンなし)',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5.0),
-                  ),
+                decoration: const InputDecoration(
+                  hintText: '電話番号を入力(ハイフンなし)',
+                  border: OutlineInputBorder(),
                 ),
                 keyboardType: TextInputType.number,
                 inputFormatters: [FilteringTextInputFormatter.digitsOnly],
@@ -81,9 +73,7 @@ class _LoginPageState extends State<LoginPage> {
                 controller: _passWord,
                 decoration: InputDecoration(
                   hintText: 'パスワードを入力',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5.0),
-                  ),
+                  border: const OutlineInputBorder(),
                   suffixIcon: IconButton(
                     icon: Icon(
                       _hidePassword ? Icons.visibility : Icons.visibility_off,
@@ -95,32 +85,14 @@ class _LoginPageState extends State<LoginPage> {
                     },
                   ),
                 ),
-                obscureText: !_hidePassword,
+                obscureText: _hidePassword,
                 keyboardType: TextInputType.number,
                 inputFormatters: [FilteringTextInputFormatter.digitsOnly],
               ),
               const SizedBox(height: 24),
               ElevatedButton(
-                onPressed: _isLoginButton ? _login : null,
-                child: const Text('ログイン'),
-              ),
-              TextButton(
-                onPressed: () {},
-                child: const Text('パスワードを忘れてしまった場合'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const RegistrationPage()),
-                  );
-                },
-                child: const Text('新規会員登録'),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text('以前会員登録した方はこちら'),
+                onPressed: _isResistrationButton ? _register : null,
+                child: const Text('会員登録'),
               ),
             ],
           ),
