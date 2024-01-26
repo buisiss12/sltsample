6c9394c1c47c502a1e487a5bc3b7a8358e8f75e1
nyny <buisiss12@gmail.com>
2024-01-26 15:15:32 +0900

    プロフィールページ修正

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // used in 'addpost_page.dart'
@@ -17,3 +19,12 @@ final birthdayProvider = StateProvider.autoDispose<DateTime?>((ref) => null);
 
 // used in 'forgetpw_page.dart' ''
 final memberNumberProvider = StateProvider.autoDispose<String>((ref) => '');
+
+final userProfileProvider =
+    FutureProvider.autoDispose<DocumentSnapshot>((ref) async {
+  User? user = FirebaseAuth.instance.currentUser;
+  if (user == null) {
+    throw Exception('ユーザーがログインしていません');
+  }
+  return FirebaseFirestore.instance.collection('users').doc(user.uid).get();
+});
