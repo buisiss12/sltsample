7cb7b702270f6e4eca9692a24cec85b53d5f2a17
nyny <buisiss12@gmail.com>
2024-02-07 10:54:02 +0900

    掲示板投稿ページのメソッドを別クラスへ

--- lib/models/model.dart
+++ lib/models/model.dart
@@ -3,6 +3,101 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_datetime_picker_plus/flutter_datetime_picker_plus.dart';
 
 class Logics {
+  static const List<String> todohuken = [
+    '北海道',
+    '青森県',
+    '岩手県',
+    '宮城県',
+    '秋田県',
+    '山形県',
+    '福島県',
+    '茨城県',
+    '栃木県',
+    '群馬県',
+    '埼玉県',
+    '千葉県',
+    '東京都',
+    '神奈川県',
+    '新潟県',
+    '富山県',
+    '石川県',
+    '福井県',
+    '山梨県',
+    '長野県',
+    '岐阜県',
+    '静岡県',
+    '愛知県',
+    '三重県',
+    '滋賀県',
+    '京都府',
+    '大阪府',
+    '兵庫県',
+    '奈良県',
+    '和歌山県',
+    '鳥取県',
+    '島根県',
+    '岡山県',
+    '広島県',
+    '山口県',
+    '徳島県',
+    '香川県',
+    '愛媛県',
+    '高知県',
+    '福岡県',
+    '佐賀県',
+    '長崎県',
+    '熊本県',
+    '大分県',
+    '宮崎県',
+    '鹿児島県',
+    '沖縄県',
+  ];
+  static Future<void> show(BuildContext context,
+      ValueNotifier<List<String>> selectedTodohuken) async {
+    final List<String> selectedValues = List.from(selectedTodohuken.value);
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('都道府県を選択'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: todohuken.map((String prefecture) {
+                return CheckboxListTile(
+                  value: selectedValues.contains(prefecture),
+                  title: Text(prefecture),
+                  onChanged: (bool? value) {
+                    if (value == true) {
+                      if (!selectedValues.contains(prefecture)) {
+                        selectedValues.add(prefecture);
+                      }
+                    } else {
+                      selectedValues.remove(prefecture);
+                    }
+                    (context as Element).markNeedsBuild();
+                  },
+                );
+              }).toList(),
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text("キャンセル"),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                selectedTodohuken.value = selectedValues;
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   static Future<void> selectBirthday(
       BuildContext context, StateController<DateTime?> birthdayNotifier) async {
     final DateTime? picked = await DatePicker.showDatePicker(
--- lib/screens/after_login/addpost_page.dart
+++ lib/screens/after_login/addpost_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:sltsampleapp/models/model.dart';
 import 'package:sltsampleapp/models/post_model.dart';
 import 'package:sltsampleapp/screens/after_login/solotte_page.dart';
 import '../../provider/provider.dart';
@@ -15,101 +16,6 @@ class AddPostPage extends HookConsumerWidget {
     final postTitle = useState('');
 
     final selectedTodohuken = useState<List<String>>([]);
-
-    const List<String> todohuken = [
-      '北海道',
-      '青森県',
-      '岩手県',
-      '宮城県',
-      '秋田県',
-      '山形県',
-      '福島県',
-      '茨城県',
-      '栃木県',
-      '群馬県',
-      '埼玉県',
-      '千葉県',
-      '東京都',
-      '神奈川県',
-      '新潟県',
-      '富山県',
-      '石川県',
-      '福井県',
-      '山梨県',
-      '長野県',
-      '岐阜県',
-      '静岡県',
-      '愛知県',
-      '三重県',
-      '滋賀県',
-      '京都府',
-      '大阪府',
-      '兵庫県',
-      '奈良県',
-      '和歌山県',
-      '鳥取県',
-      '島根県',
-      '岡山県',
-      '広島県',
-      '山口県',
-      '徳島県',
-      '香川県',
-      '愛媛県',
-      '高知県',
-      '福岡県',
-      '佐賀県',
-      '長崎県',
-      '熊本県',
-      '大分県',
-      '宮崎県',
-      '鹿児島県',
-      '沖縄県',
-    ];
-
-    void showPrefectureSelectionDialog() async {
-      final List<String> selectedValues = List.from(selectedTodohuken.value);
-      await showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text('都道府県を選択'),
-            content: SingleChildScrollView(
-              child: ListBody(
-                children: todohuken.map((String prefecture) {
-                  return CheckboxListTile(
-                    value: selectedValues.contains(prefecture),
-                    title: Text(prefecture),
-                    onChanged: (bool? value) {
-                      if (value == true) {
-                        if (!selectedValues.contains(prefecture)) {
-                          selectedValues.add(prefecture);
-                        }
-                      } else {
-                        selectedValues.remove(prefecture);
-                      }
-                      (context as Element).markNeedsBuild();
-                    },
-                  );
-                }).toList(),
-              ),
-            ),
-            actions: <Widget>[
-              TextButton(
-                child: const Text("キャンセル"),
-                onPressed: () => Navigator.of(context).pop(),
-              ),
-              TextButton(
-                child: const Text('OK'),
-                onPressed: () {
-                  selectedTodohuken.value = selectedValues;
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        },
-      );
-    }
 
     Future<void> addPost() async {
       final auth = ref.watch(firebaseAuthProvider);
@@ -139,7 +45,7 @@ class AddPostPage extends HookConsumerWidget {
         children: <Widget>[
           const Text('希望地域'),
           TextField(
-            onTap: () => showPrefectureSelectionDialog(),
+            onTap: () => Logics.show(context, selectedTodohuken),
             controller:
                 TextEditingController(text: selectedTodohuken.value.join(', ')),
             readOnly: true,
