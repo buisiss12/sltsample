4c0fd00c4a7a872879ffaf10b7a535e92e9a27c8
nyny <buisiss12@gmail.com>
2024-02-29 03:17:48 +0900

    プロフィール表示ロジック変更

--- lib/screens/home_widget/pages/my_page.dart
+++ lib/screens/home_widget/pages/my_page.dart
@@ -13,108 +13,111 @@ class MyPage extends ConsumerWidget {
 
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(10.0),
+        padding: const EdgeInsets.all(16.0),
         child: userStateFuture.when(
-          data: (userList) => ListView.builder(
-            itemCount: userList.length,
-            itemBuilder: (context, index) {
-              final user = userList[index];
+          data: (users) {
+            final user = users.isNotEmpty ? users.first : null;
+            if (user != null) {
               final age = Utility.birthdayToAgeConverter(user.birthday);
-              return Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          CircleAvatar(
-                            radius: 40,
-                            backgroundImage: user.profileImageUrl.isNotEmpty
-                                ? NetworkImage(user.profileImageUrl)
-                                : const AssetImage(
-                                        'assets/images/300x300defaultprofile.png')
-                                    as ImageProvider,
-                          ),
-                          ElevatedButton(
-                            onPressed: () {
-                              Navigator.of(context).push(
-                                MaterialPageRoute(
-                                  builder: (context) {
-                                    return EditProfilePage(user: user);
-                                  },
-                                ),
-                              );
-                            },
-                            child: const Text('プロフィールを編集'),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 16),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+              return SingleChildScrollView(
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
-                            Row(
-                              children: [
-                                const Icon(Icons.person),
-                                const SizedBox(width: 8),
-                                Text('ニックネーム: ${user.nickName}'),
-                              ],
+                            CircleAvatar(
+                              radius: 40,
+                              backgroundImage: user.profileImageUrl.isNotEmpty
+                                  ? NetworkImage(user.profileImageUrl)
+                                  : const AssetImage(
+                                          'assets/images/300x300defaultprofile.png')
+                                      as ImageProvider,
                             ),
-                            const Divider(),
-                            Row(
-                              children: [
-                                const Icon(Icons.transgender),
-                                const SizedBox(width: 8),
-                                Text('性別: ${user.gender}'),
-                              ],
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) {
+                                      return EditProfilePage(user: user);
+                                    },
+                                  ),
+                                );
+                              },
+                              child: const Text('プロフィールを編集'),
                             ),
-                            const Divider(),
-                            Row(
-                              children: [
-                                const Icon(Icons.cake),
-                                const SizedBox(width: 8),
-                                Text('年齢: $age'),
-                              ],
-                            ),
-                            const Divider(),
-                            Row(
-                              children: [
-                                const Icon(Icons.height),
-                                const SizedBox(width: 8),
-                                Text('身長: ${user.height}'),
-                              ],
-                            ),
-                            const Divider(),
-                            Row(
-                              children: [
-                                const Icon(Icons.work),
-                                const SizedBox(width: 8),
-                                Text('職業: ${user.job}'),
-                              ],
-                            ),
-                            const Divider(),
-                            Row(
-                              children: [
-                                const Icon(Icons.home),
-                                const SizedBox(width: 8),
-                                Text('居住地: ${user.residence}'),
-                              ],
-                            ),
-                            const Divider(),
                           ],
                         ),
-                      )
-                    ],
+                        const SizedBox(height: 16),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  const Icon(Icons.person),
+                                  const SizedBox(width: 8),
+                                  Text('ニックネーム: ${user.nickName}'),
+                                ],
+                              ),
+                              const Divider(),
+                              Row(
+                                children: [
+                                  const Icon(Icons.transgender),
+                                  const SizedBox(width: 8),
+                                  Text('性別: ${user.gender}'),
+                                ],
+                              ),
+                              const Divider(),
+                              Row(
+                                children: [
+                                  const Icon(Icons.cake),
+                                  const SizedBox(width: 8),
+                                  Text('年齢: $age'),
+                                ],
+                              ),
+                              const Divider(),
+                              Row(
+                                children: [
+                                  const Icon(Icons.height),
+                                  const SizedBox(width: 8),
+                                  Text('身長: ${user.height}'),
+                                ],
+                              ),
+                              const Divider(),
+                              Row(
+                                children: [
+                                  const Icon(Icons.work),
+                                  const SizedBox(width: 8),
+                                  Text('職業: ${user.job}'),
+                                ],
+                              ),
+                              const Divider(),
+                              Row(
+                                children: [
+                                  const Icon(Icons.home),
+                                  const SizedBox(width: 8),
+                                  Text('居住地: ${user.residence}'),
+                                ],
+                              ),
+                              const Divider(),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               );
-            },
-          ),
+            } else {
+              return const Center(child: Text('ユーザーデータがありません。'));
+            }
+          },
           loading: () => const Center(child: CircularProgressIndicator()),
-          error: (error, stackTrace) => Center(child: Text('Error: $error')),
+          error: (e, stack) => Center(child: Text('エラーが発生しました: $e')),
         ),
       ),
     );
